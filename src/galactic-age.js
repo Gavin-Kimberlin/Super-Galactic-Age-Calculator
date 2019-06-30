f4623b63497632aa2eec25a38a825306418ae444
Gavin Kimberlin <guviner@gmail.com>
2019-06-30 01:41:39 -0700

    add prototypes for objects.

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -18,7 +18,32 @@ export function Mars(howOld, expextedAge) {
   this.expectedAge = expectedAge;
 };
 
-export function (howOld, expextedAge) {
+export function Jupiter(howOld, expextedAge) {
   this.howOld = howOld;
   this.expectedAge = expectedAge;
+};
+
+Age.prototype = function (ageInput, expectedAgeInput) {
+  this.howOld = ageInput;
+  this.expectedAge = expectedAgeInput;
+};
+
+Mercury.prototype = function (ageInput, expectedAgeInput) {
+  this.howOld = ageInput / 0.24;
+  this.expectedAge = expectedAgeInput / 0.24;
+};
+
+Venus.prototype = function (ageInput, expectedAgeInput) {
+  this.howOld = ageInput / 0.62;
+  this.expectedAge = expectedAgeInput / 0.62;
+};
+
+Mars.prototype = function (ageInput, expectedAgeInput) {
+  this.howOld = ageInput / 1.88;
+  this.expectedAge = expectedAgeInput / 1.88;
+};
+
+Jupiter.prototype = function (ageInput, expectedAgeInput) {
+  this.howOld = ageInput / 11.86;
+  this.expectedAge = expectedAgeInput / 11.86;
 };
