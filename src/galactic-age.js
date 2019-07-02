5f000c3bfec7d28b6579a5d4cc734a01bebaa540
Gavin Kimberlin <guviner@gmail.com>
2019-07-02 09:51:11 -0700

    add all logic to get the age on other planets.

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -6,38 +6,32 @@ export default class Planets {
     this.mars = [age, expectedAge];
     this.jupiter = [age, expectedAge];
     this.mercuryLogic();
+    this.venusLogic();
+    this.marsLogic();
+    this.jupiterLogic();
   };
+
   mercuryLogic() {
     this.mercury = this.mercury.map(function (mercuryAge) {
       return mercuryAge / 0.24;
     });
-  }
+  };
+ 
   venusLogic() {
-    this.venus /= 0.62;
-  }
-};
+    this.venus = this.venus.map(function (venusAge) {
+      return venusAge / 0.62;
+    });
+  };
 
-// Age.prototype = function (ageInput, expectedAgeInput) {
-//   this.howOld = ageInput;
-//   this.expectedAge = expectedAgeInput;
-// };
-//
-// Mercury.prototype = function (ageInput, expectedAgeInput) {
-//   this.howOld = ageInput / 0.24;
-//   this.expectedAge = expectedAgeInput / 0.24;
-// };
-//
-// Venus.prototype = function (ageInput, expectedAgeInput) {
-//   this.howOld = ageInput / 0.62;
-//   this.expectedAge = expectedAgeInput / 0.62;
-// };
-//
-// Mars.prototype = function (ageInput, expectedAgeInput) {
-//   this.howOld = ageInput / 1.88;
-//   this.expectedAge = expectedAgeInput / 1.88;
-// };
-//
-// Jupiter.prototype = function (ageInput, expectedAgeInput) {
-//   this.howOld = ageInput / 11.86;
-//   this.expectedAge = expectedAgeInput / 11.86;
-// };
+  marsLogic() {
+    this.mars = this.mars.map(function (marsAge) {
+      return marsAge / 1.88;
+    });
+  };
+
+  jupiterLogic() {
+    this.jupiter = this.jupiter.map(function (jupiterAge) {
+      return jupiterAge / 11.86;
+    });
+  };
+};
