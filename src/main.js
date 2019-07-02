c659dece324b41d05477f777067b009ca1859b43
Gavin Kimberlin <guviner@gmail.com>
2019-07-02 10:42:38 -0700

    results now print to the webpage.

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
     const ageInput = $('#age').val();
     const expectedAgeInput = $('#expectedAge').val();
     let newPlanets = new Planets(ageInput, expectedAgeInput);
+    $('#result').html('Your age on earth: ' + newPlanets.earth[0] + '<br>Your expected lifespan on Earth: ' + newPlanets.earth[1] + '<br> Your age on Mercury: ' + newPlanets.mercury[0] + '<br> Your expected lifespan on Mercury: ' + newPlanets.mercury[1] + '<br> Your age on Venus: ' + newPlanets.venus[0] + '<br> Your expected lifespan on Venus: ' + newPlanets.venus[1] + '<br> Your age on Mars: ' + newPlanets.mars[0] + '<br> Your expected lifespan on Mars: ' + newPlanets.mars[1] + '<br> Your age on Jupiter: ' + newPlanets.jupiter[0] + '<br> Your expected lifespan on Jupiter: ' + newPlanets.jupiter[1]);
     console.log(newPlanets);
   });
 });
